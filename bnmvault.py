a1fa86a97ebeb52923a2c13eab34b15bc353fa72
Neha Deshpande <106668510+nehadesh933@users.noreply.github.com>
2024-08-01 21:05:09 +0530

    Update bnmvault.py

--- bnmvault.py
+++ bnmvault.py
@@ -8,7 +8,7 @@ st.title("BNM VAULT")
 
 def connect_db():
     # Use the MongoDB Atlas connection string
-    conn = pymongo.MongoClient("mongodb+srv://nehadesh2003:%3Cpassword%3E@cluster.txjjqct.mongodb.net/")
+    conn = pymongo.MongoClient("mongodb+srv://nehadesh2003:%3Cmangocluster6%3E@cluster.txjjqct.mongodb.net/")
     db = conn['bnmvault']  # Use the database name that matches your MongoDB Atlas setup
     return db
 
